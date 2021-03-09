ce2052c1613f6dd56567250210193404d06167ab
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-09 10:44:36 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import './style.css';
+
+//https://sujeitoprogramador.com/rn-api/?api=posts
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            nutri:  []
+        }
+    }
+
+    componentDidMount(){
+       let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+       fetch(url)
+       .then((r) => r.json())
+       .then((json)=>{
+           let state = this.state;
+           state.nutri = json;
+           this.setState(state);
+       });
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <header>
+                    <strong>React Nutri</strong>
+                </header>
+                {this.state.nutri.map( (item) => {
+                    return(
+                        <article key={item.id} className="post">
+                            <strong className="titulo">{item.titulo}</strong>
+                            <img className="capa" src={item.capa} alt={item.titulo}/>
+                            <p className="subtitulo">{item.subtitulo}</p>
+                            <a className="botao" href="#">LEIA MAIS</a>
+                        </article>
+                    );
+                })}
+            </div>
+        );
+    }
+}
+
+export default App;
